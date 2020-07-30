70b13481fe8416bb05616867ad5a85a79a9f54d3
nelson Hernandez Guerra <nelsoneo@gmail.com>
2020-07-29 22:45:44 -0300

    Create aula#2.js

---
+++ Modulo A/aula#2.js
@@ -0,0 +1,34 @@
+/* 
+1970 - Arpanet inicio de la INTERNET
+
+1993 - CERN surge a WWW e surge o navegador 
+
+1994 - Netscape navgador mais famoso na epoca
+
+1995 - nace javascript(JS)
+
+1997 - ECMAScript padronizado 
+
+2002 - nace Firefox
+
+2008 - GoogleCrome
+
+2009 - V8 motor que roda js no googlecrome
+
+2010 - Node JS
+------------------------------------------------------------
+
+ESMAScript
+
+1997 - V 1.0            2016 - ES
+1998 - V 2.0            2017 - ES
+1999 - V 3.0            2018 - ES
+2009 - ES5
+2015 - ES6
+------------------------------------------------------------
+
+Tecnologias que usam JS
+
+1. Jquery   2.Angular   3.React    4.Viu     5. Electron
+
+6. Aionic   7.Concordova

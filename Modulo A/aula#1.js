ac7b0262de58a75ca9111a8ece355ff3add7880f
nelson Hernandez Guerra <nelsoneo@gmail.com>
2020-07-29 22:07:20 -0300

    mais aula#1

--- Modulo A/aula#1.js
+++ Modulo A/aula#1.js
@@ -9,3 +9,6 @@ document.querySelector('img#hplogo').style.filter = 'grayscale(100$)';
 
 //esconder o logo do google no navegador
 document.querySelector('img#hplogo').style.visibility = 'hidden';
+
+//mudar um texto de um site no nevegador
+document.querySelector('o que vai mudar').innerText = 'texo que vai reemplazar';

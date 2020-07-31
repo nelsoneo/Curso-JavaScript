3de349f47e2959b73f4afca7e892cff436933793
nelson Hernandez Guerra <nelsoneo@gmail.com>
2020-07-30 22:42:10 -0300

    modulo B

---
+++ Modulo B/aula#5.js
@@ -0,0 +1,37 @@
+//Variável
+/* -/OJO\- NO JAVASCRIPT moderno, além de utilizar a palavra 'var', também podemos usar a palavra 'let'*/
+
+var ai = carro1 //onde '=' toma o nome de (recebe)
+
+let ai = null //significa que 'a1' não vai ter nada (null), onde 'a1' se tenha algum valor, agora foi destruido
+
+//----------------------------------------------------------
+//Nota: Existe 3 formar de delimitar um string dentro de javascript, que é usuando ("aspas dobles"), 'aspas simples', `acrase`
+
+var a1 = "nelson"
+var a2 = 'hernandez'
+let a3 = `guerra`
+
+//REGRAS PARA IDENTIFICADORES de VARIAVEL \*OJO*/
+/*
+#Podem começar com letra, $ ou _
+#Não podem começar com números
+#É possível usar letras ou números
+#É possível usar acentos e símbolos
+#Não podem conter espaços
+#Não podem ser palavras reservadas
+ */
+
+ //Dicas muito Importantes!!!
+ /*
+ # Maiúsculas e minúsculas fazem diferença
+ # Tente escolher nomes coerentes para as variáveis
+ # Evite se tornar um 'programador alfaveto' ou um 'programador contador'
+ */
+
+ //TIPOS PRIMITIVOS DE VARIAVEL\\
+ /**DATA TYPES
+  - number
+  - string
+  - boolean
+ */

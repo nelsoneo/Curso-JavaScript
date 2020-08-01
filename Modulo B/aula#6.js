38d74e7261e9467230961e3d16c077279d7f1765
nelson Hernandez Guerra <nelsoneo@gmail.com>
2020-07-31 22:53:40 -0300

    add

--- Modulo B/aula#6.js
+++ Modulo B/aula#6.js
@@ -25,3 +25,18 @@ var s = JavaScript
 n.length //--- quantos caracteres a string tem
 n.toUpperCase() //--- tudo para 'MAIÚSCULAS'
 n.toLowerCase() //-----tudo para 'minúsculas'
+
+//----------------------------------------------------------------------------------------------------------------
+//FORMATAR NUMEROS
+n1 = 1545.5
+
+//para agregar duas casas no decimal podemos usar
+n1.toFixed(2) // 1545.50
+//si quiser trocar o ponto (.) pela virgula (,) seria
+n1.toFixed(2).replace('.', ',') //1545,50
+
+//OJO
+//Uma dica para botar o numero em moneda e valor do pais que quera
+n1.toLocaleString('pt-Br', {style: 'currency', currency: 'BRL'}) // R$1,545.50
+n1.toLocaleString('pt-Br', {style: 'currency', currency: 'USD'}) // $1,545.50
+n1.toLocaleString('pt-Br', {style: 'currency', currency: 'EUR'}) // £1,545.50

de5f77191d38c0189f6a1311a886714529529ed1
nelson Hernandez Guerra <nelsoneo@gmail.com>
2020-07-31 22:06:39 -0300

    mais

--- Modulo B/aula#6.js
+++ Modulo B/aula#6.js
@@ -2,6 +2,16 @@
 
 //convertir variáveis de um tipo para outro
 
+//para convertir de string a numero
 Number.parseInt()//converte para número
 Number.parseFloat()//converte para float
 //--------dá para usar também só parseint() ou parsefloat()
+//OJO 
+//hoje na atualidade só usuando Number(var), o javascript buscara a melhor interpretação da variavel para number o float
+
+//----------------------------------------------------------------------------------------------------------------
+
+/*para convertir de numero a string*/
+
+String(n);
+numero.toString()

9f11c26a8d9389e2623feecd50a63407133d6450
nelson Hernandez Guerra <nelsoneo@gmail.com>
2020-07-31 21:52:23 -0300

    Update aula#6.js

--- Modulo B/aula#6.js
+++ Modulo B/aula#6.js
@@ -1,1 +1,7 @@
 //MANIPULAÇÃO DE DADOS
+
+//convertir variáveis de um tipo para outro
+
+Number.parseInt()//converte para número
+Number.parseFloat()//converte para float
+//--------dá para usar também só parseint() ou parsefloat()

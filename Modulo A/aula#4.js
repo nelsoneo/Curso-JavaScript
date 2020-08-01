ffdfb847d1db44aa9866efd09944a4e5654a8461
nelson Hernandez Guerra <nelsoneo@gmail.com>
2020-07-31 21:44:31 -0300

    add

--- Modulo A/aula#4.js
+++ Modulo A/aula#4.js
@@ -1,3 +1,5 @@
-//No javascript vamos utilizar o simbolo de '+' para concatenar string com variáveis por exemplo
+//No javascript vamos utilizar o simbolo de '+' para sumar e para concatenar, por exemplo:
+
+//string  + string -> Concatenacion
 var nome = window.prompt('Qual é seu nome?')
         alert('Seu nome é '+ nome);
